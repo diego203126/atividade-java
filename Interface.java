ede1ac4eab827a59ff8184f4fef2aef75c7d9dd8
diego sena <diegosenadeoliveira88@gmail.com>
2025-06-04 15:29:16 -0300

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,12 @@
+package MeuProjeto;
+
+public class Interface {
+    public static void main(String[] args) {
+
+
+        interface indicador{
+            double CalcularIndicador();
+
+        }
+    }
+}
